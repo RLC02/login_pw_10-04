26679272a54bada3f925f2bcea8e485969075b8c
Ricardo Luquetti <99838772+RLC02@users.noreply.github.com>
2023-04-17 22:19:26 -0300

    update

--- Login/cadastro.php
+++ Login/cadastro.php
@@ -4,19 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-	<title>Login</title>
+	<title>Cadastro</title>
 </head>
 <body>
-	<h1>Login</h1>
-	<form method="post" action="gerencia-login.php">
+	<h1>Cadastro</h1>
+	<form method="post" action="gerencia-cadastro.php">
 		<label for="Email">Email:</label>
 		<input type="email" id="Email" name="Email" required>
 		<br>
 		<label for="password">Password:</label>
 		<input type="password" id="password" name="password" required>
 		<br>
-		<input type="submit" value="Login">
+		<input type="submit" value="Cadastrar">
 	</form>
-	<a href="cadastro.php">Cadastre-se</a>
 </body>
 </html>
